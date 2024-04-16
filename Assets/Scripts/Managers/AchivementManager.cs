085445f98f163311cbfa5ed89ef9a67df709199d
Deus Ex Machina <quiocdat777@gmail.com>
2024-04-16 10:29:05 +0700

    neft trap hitbox

--- Assets/Scripts/Managers/AchivementManager.cs
+++ Assets/Scripts/Managers/AchivementManager.cs
@@ -8,6 +8,9 @@ public class AchivementManager : MonoBehaviour
     private int kills;
     private int score;
     private int remainTime;
+    private int apple;
+    private int banana;
+    private int melon;
     private void Awake()
     {
         if (Instance != null && Instance != this)
@@ -24,16 +27,33 @@ public class AchivementManager : MonoBehaviour
         Enemy.OnDead += AddKill;
         Scoreable.OnScoreChanged += AddScore;
         Timer.OnTimerStop += GetTimeRemain;
+        Fruit.OnCollected += AddFruit;
     }
     private void OnDisable()
     {
         Enemy.OnDead -= AddKill;
         Scoreable.OnScoreChanged -= AddScore;
         Timer.OnTimerStop -= GetTimeRemain;
+        Fruit.OnCollected -= AddFruit;
     }
     private void AddKill() => kills++;
     private void AddScore(int value) => score += value;
     private void GetTimeRemain(int value) => remainTime = value;
+    private void AddFruit(ItemType fruitType)
+    {
+        switch (fruitType)
+        {
+            case ItemType.Apple:
+                apple++;
+                break;
+            case ItemType.Banana:
+                banana++;
+                break;
+            case ItemType.Melon:
+                melon++;
+                break;
+        }
+    }
     public int GetBonus()
     {
         return remainTime * 100;
